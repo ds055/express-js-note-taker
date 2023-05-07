e5a4b592a36f60846874c8095392a2011949e372
Drew Smith <drgh056@gmail.com>
2023-05-07 16:10:43 -0500

    Cleaned up server.js; Added comments

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # express-js-note-taker
+
+
+
+
+
+
+https://stackoverflow.com/questions/48163429/remove-json-object-from-json-array-element
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+// Pull in required modules/functions
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+// Holds read/write functions to help clean up this file
 const { readFilePromise, writeToFile, appendData } = require('./utils/utilities');
+// module that generates unique id
 const uuid = require('uuid');
 
+// Establish port variable
 const PORT = process.env.PORT || 3001;
 
+// Establish express variable
 const app = express();
 
+// Set up data types
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Set up public folder
 app.use(express.static('public'));
 
 // Sends notes.html file based on /notes path
@@ -21,21 +28,21 @@ app.get('/notes', (req, res) => {
 // GET /api/notes should return db.json all notes as json
 app.get('/api/notes', async (req, res) => {
     try {
-        // Read db from file; must do this every call since page dynamically updates
+        // Read db from file; must do this every call since page dynamically updates; uses method from utilities.js
         const dbString = await readFilePromise('./db/db.json');
 
-        // Parse into JSON to send in response
+        // Parse db into JSON to send in response
         const parsedDB = JSON.parse(dbString);
 
-        // Send JSON db back
+        // Send json db back
         res.json(parsedDB);
     } catch(err){
-        // Send back error message if get fails
+        // Send back error message if GET fails
         res.json(err);
     }
 })
 
-// Add new post
+// POST /api/notes addes new note to json db
 app.post('/api/notes', async (req, res) => {
     try {
          // Destructure note from req.body
@@ -46,61 +53,42 @@ app.post('/api/notes', async (req, res) => {
             return console.log("Missing note component.")
         }
 
-        // New note variable
+        // New note variable object
         const newNote = {
             title, 
             text, 
             id: uuid.v4()
         };
 
+        // Call appendData function from utilities.js; pushes new note into array, writes to file, returns db as json for res below
         const newNoteList = await appendData(newNote, './db/db.json');
 
+        // Send updated object array to user as json
         res.json(newNoteList)
 
-
-        // // Get notes db
-        // const dbString = await fs.promises.readFile('./db/db.json')
-
-        // // Parse db
-        // const parsedDB = JSON.parse(dbString)
-
-        // // Add note to parsed data
-        // parsedDB.push(newNote);
-
-        // // Stringify parsedDB
-        // const restrungDB = JSON.stringify(parsedDB, null, 4);
-
-        // // Write parse data to file
-        // fs.writeFile(
-        // './db/db.json',
-        // restrungDB,
-        // (err) => err ? console.log(err): console.log("Success!"));
-
-        // res.json(parsedDB);
-        
     } catch(err) {
         res.json(err);
     }
 })
 
-// remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+// Remove note with the given `id` property, and then rewrite remaining notes to the `db.json` file.
 app.delete('/api/notes/:id', async (req, res) => {
     try {
-        // id of note to delete based on api request
+        // Id of note to delete based on api request
         const noteId = req.params.id;
 
-        // Get db in string form
+        // Get db in string form via function from utilities.js
         const dbString = await readFilePromise('./db/db.json');
 
         // Parse db into json
         const dbParse = JSON.parse(dbString);
 
-        // new array with selected note removed
+        // Adds notes to new array (dbElRemoved) that do not match id to be removed; return array with remaining notes
         const dbElRemoved = dbParse.filter((el) => {
             return el.id !== noteId;
         })
 
-        // Write new note list to file
+        // Write new note list to file using method from utilities.js
         await writeToFile('./db/db.json', dbElRemoved);
 
         // New json array returned in res
@@ -112,11 +100,12 @@ app.delete('/api/notes/:id', async (req, res) => {
     }
 })
 
-// Any other route re-directs to index page
+// Any other / route re-directs to index page
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+// Open ports
 app.listen(PORT, (err) => {
     if (err) console.log(err);
     console.log(`App listening on Port ${PORT}`);
